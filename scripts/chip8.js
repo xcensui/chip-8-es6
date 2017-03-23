7ae1df8bb0faefeaf5ed32cf177d4bf295054f0f
Paul Race <dynasty1002@ntlworld.com>
2017-03-23 21:18:56 +0000

    Reworked single counter values, started emulation cycle and opcode handling

--- scripts/chip8.js
+++ scripts/chip8.js
@@ -5,29 +5,28 @@ class Chip8 {
 	}
 
 	getOpcode() {
-		return this.memory[this.progCounter[0]] << 8 | this.memory[this.progCounter[0] + 1];
+		return this.memory[this.counters[1]] << 8 | this.memory[this.counters[1] + 1];
 	}
 
 	start() {
 		this.running = true;
-
-		window.requestAnimationFrame(this.run());
+		window.requestAnimationFrame(() => this.run());
 	}
 
 	run() {
 		if (this.running) {
 			for (var i = 0; i < 10; i++) {
-				console.log(this.getOpcode());
-				this.progCounter[0] += 2;
+				this.doCycle();
 			}			
 
-			this.running = false;
-		}		
-		window.requestAnimationFrame(this.run());
+			this.stop();
+		
+			window.requestAnimationFrame(() => this.run());
+		}
 	}
 
-	doCycle() {
-
+	stop() {
+		this.running = false;
 	}
 
 	reset() {
@@ -37,10 +36,7 @@ class Chip8 {
 		this.stack = new Uint16Array(new ArrayBuffer(0x10));
 		this.reg = new Uint8Array(new ArrayBuffer(0x10));
 		this.keyState = new Uint8Array(new ArrayBuffer(0x10));
-		this.opcode = new Uint16Array(1);
-		this.progCounter = new Uint16Array(1);
-		this.addrReg = new Uint16Array(1);
-		this.timer = new Uint16Array(1);
+		this.counters = new Uint16Array(new ArrayBuffer(4)); //Holds opcode, progCounter, addrReg, and timer.
 		this.sound = new Chip8Sound();
 		this.display = new Chip8Display();
 		this.running = false;
@@ -51,12 +47,12 @@ class Chip8 {
 			this.memory[i] = font[i];
 		}
 
-		console.log(this.memory);
+		this.counters[0] = 0;		//Opcode
+		this.counters[1] = 0x200;	//PC
+		this.counters[2] = 0;		//Reg
+		this.counters[3] = 0;		//Timer
 
-		this.progCounter[0] = 0x200;
-		this.addrReg[0] = 0;
-		this.timer[0] = 0;
-		this.opcode[0] = 0;
+		console.log(this.counters);
 	}
 
 	systemFont() {
@@ -78,5 +74,69 @@ class Chip8 {
             0xF0, 0x80, 0xF0, 0x80, 0xF0,
             0xF0, 0x80, 0xF0, 0x80, 0x80
 		];
+	}
+
+	doCycle() {
+		this.counters[0] = this.getOpcode();
+		this.counters[1] += 2;
+
+		this.handleOpcode();
+	}
+
+	opcodeNoSupport(source) {
+		throw new Error('['+source+'] Opcode '+this.counters[0].toString(16)+' is not supported.');
+	}
+
+	handleOpcode() {
+		switch(this.counters[0] & 0xF000) {
+			case 0x0000:
+				this.handleOpcode0();
+				break;
+			case 0x8000:
+				this.handleOpcode8();
+				break;
+			case 0xE000:
+				this.handleOpcodeE();
+				break;
+			case 0xF000:
+				this.handleOpcodeF();
+				break;
+			default:
+				this.opcodeNoSupport('Opcode');
+		}
+	}
+
+	handleOpcode0() {
+		switch(this.counters[0] & 0x00FF) {
+			case 0x00E0:
+				this.OpcodeE0();
+				break;
+			case 0x00EE:
+				this.OpcodeEE();
+				break;
+			default:
+				this.opcodeNoSupport('Opcode0');
+		}
+	}
+
+	handleOpcode8() {
+		switch(this.counter[0] & 0x000F) {
+			default:
+				this.opcodeNoSupport('Opcode8');
+		}
+	}
+
+	handleOpcodeE() {
+		switch(this.counter[0] & 0x000F) {
+			default:
+				this.opcodeNoSupport('Opcode8');
+		}
+	}
+
+	handleOpcodeF() {
+		switch(this.counter[0] & 0x00FF) {
+			default:
+				this.opcodeNoSupport('Opcode8');
+		}
 	}
 }
